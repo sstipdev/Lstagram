3eda6bf0dd7aa6aedd8f0daf8de4e26f3dd8d5a5
lk1 <whitecandy777@naver.com>
2023-09-20 22:19:33 +0900

    Next Session Context api 추가

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+// Next
+import { SessionProvider } from "next-auth/react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function AuthContext({ children }: Props) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
